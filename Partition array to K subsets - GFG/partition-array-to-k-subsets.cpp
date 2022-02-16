9b28e91040472e132b89ad50a6fc600cc2d4e2e7
cheeragmishra9431 <58364680+cheeragmishra9431@users.noreply.github.com>
2022-02-16 11:11:14 +0530

    Added solution - LeetHub

---
+++ Partition array to K subsets - GFG/partition-array-to-k-subsets.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution{
+  public:
+     bool helper(int sum,int s,int k,bool arr[],int ind,int n,int a[]){
+        if(k == 1)
+            return true;
+        if(sum > s)
+            return false;
+        if(sum == s){
+            return helper(0,s,k - 1,arr,0,n,a);
+        }
+        for(int i  = ind;i < n ; i ++){
+            if(arr[i] == false){
+                arr[i] = true;
+                if(helper(sum + a[i],s,k,arr,i + 1,n,a))
+                    return true;
+                else  arr[i] = false;
+            }
+        }
+        return false;
+    }
+    bool isKPartitionPossible(int a[], int n, int k){
+        if(k == 0)
+            return 0;
+        if(k == 1)  
+            return 1;
+        int sum = 0;
+        for(int i = 0; i < n ; i ++){
+            sum += a[i];
+        }
+        if(sum % k != 0) return false;
+        bool arr[n] = {false};
+        return helper(0,sum / k,k,arr,0,n,a);
+        }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	
+    	cin>>n;
+    	int a[n];
+    	for(int i=0;i<n;i++)
+    	cin>>a[i];
+    	int k;
+    	cin>>k;
+    	Solution obj;
+    	cout<<obj.isKPartitionPossible(a, n, k)<<endl;
+    }
+}  // } Driver Code Ends
